b7156fef8b120a930e49fc198f4b1f401a6b7c5b
Jens Jaeger <jaegerj204@googlemail.com>
2017-09-03 14:24:50 +0200

    format text output

--- index.js
+++ index.js
@@ -38,17 +38,61 @@ async function iterate(obj, action) {
     }
 }
 
+function textLength(obj, propName) {
+    if (obj.hasOwnProperty(propName)) {
+        const val = obj[propName];
+        if (typeof val === "string") {
+            return val.length;
+        }
+        return val.toString().length;
+    }
+}
+
+function maxLength(dataset) {
+    let res = {};
+    for(let entry of dataset) {
+        for(let key in entry) {
+            if (entry.hasOwnProperty(key)) {
+                if (res.hasOwnProperty(key)) {
+                    let max = Math.max(res[key], textLength(entry, key));
+                    res[key] = max;
+                } else {
+                    res[key] = textLength(entry, key);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+function pad(obj, prop, len) {
+    const val = obj[prop];
+    if (typeof val === "string") {
+        return val.padEnd(len[prop]);
+    }
+    return val.toFixed(2).padStart(len[prop] + 2);
+}
+
 async function computePrice(computer) {
     let sum = 0.0;
+    let data = [];
     await iterate(computer, async (part, prodId) => {
         const descr = await getProductDetails(prodId);
         const price = getPrice(descr);
         const prodName = getProductName(descr);
-        console.log(`[${part}] ${prodName}: ${price} Euro`);
+        data.push({
+            "type": part,
+            "name": prodName,
+            "price": price
+        });
         sum = sum + price;
     });
-    console.log('-------------------------------------------');
-    console.log(`Total Sum: ${sum} Euro`);
+    const lengths = maxLength(data);
+    for(let entry of data) {
+        console.log(`${pad(entry, "type", lengths)} ${pad(entry, "name", lengths)} ${pad(entry, "price", lengths)} Euro`);
+    }
+    console.log('');
+    console.log(`TOTAL SUM: ${sum.toFixed(2)} Euro`);
 }
 
 computePrice(computer);
