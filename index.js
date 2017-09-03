39e7fc573f0631be8efa0462db70becc887bbe85
Jens Jaeger <jaegerj204@googlemail.com>
2017-09-03 13:41:33 +0200

    show product name

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const fetch = require('node-fetch');
 
 const computer = require('./computer.json');
 
+async function getProductDetails(prodId) {
+    const url = `https://m.alternate.de/mobile/details.xhtml?p=${prodId}`;
+    const resp = await fetch(url);
+    const html = await resp.text();
+    const $ = cheerio.load(html);
+    return $;
+}
+
 function parsePrice(text) {
     const re = /^\D*(\d+),(\d{2})[\s\S]*/;
     let val = text.replace(re, "$1.$2");
     return new Number(val);
 }
 
-
-async function getPrice(prodId) {
-    const url = `https://m.alternate.de/mobile/details.xhtml?p=${prodId}`;
-    const resp = await fetch(url);
-    const html = await resp.text();
-    const $ = cheerio.load(html);
+function getPrice($) {
     const elem = $('.price');
     const priceText = elem.text();
     const price = parsePrice(priceText);
     return price;
 }
 
+function getProductName($) {
+    const elem = $('.productheadline');
+    const name = elem.text();
+    return name.replace(/\s\s+/g, ' ');
+}
 
 async function iterate(obj, action) {
     for(let key in obj) {
@@ -33,8 +41,10 @@ async function iterate(obj, action) {
 async function computePrice(computer) {
     let sum = 0.0;
     await iterate(computer, async (part, prodId) => {
-        let price = await getPrice(prodId);
-        console.log(`${part}: ${price} Euro`);
+        const descr = await getProductDetails(prodId);
+        const price = getPrice(descr);
+        const prodName = getProductName(descr);
+        console.log(`[${part}] ${prodName}: ${price} Euro`);
         sum = sum + price;
     });
     console.log(`Total Sum: ${sum} Euro`);
