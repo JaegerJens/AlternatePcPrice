6d3b2d02085658c4f802037072538141d71e4b11
Jens Jaeger <jaegerj204@googlemail.com>
2017-09-03 13:33:25 +0200

    sum all component prices

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch');
 const computer = require('./computer.json');
 
 function parsePrice(text) {
-    const re = /\d+,\d{2}/;
-    return re.exec(text);
+    const re = /^\D*(\d+),(\d{2})[\s\S]*/;
+    let val = text.replace(re, "$1.$2");
+    return new Number(val);
 }
 
 
@@ -17,24 +18,26 @@ async function getPrice(prodId) {
     const elem = $('.price');
     const priceText = elem.text();
     const price = parsePrice(priceText);
-    return price[0];
+    return price;
 }
 
 
-function iterate(obj, action) {
+async function iterate(obj, action) {
     for(let key in obj) {
         if (obj.hasOwnProperty(key)) {
-            action(key, obj[key]);
+            await action(key, obj[key]);
         }
     }
 }
 
 async function computePrice(computer) {
-    iterate(computer, (part, prodId) => {
-        getPrice(prodId).then(price => {
-            console.log(`${part}: ${price}`);
-        })
+    let sum = 0.0;
+    await iterate(computer, async (part, prodId) => {
+        let price = await getPrice(prodId);
+        console.log(`${part}: ${price} Euro`);
+        sum = sum + price;
     });
+    console.log(`Total Sum: ${sum} Euro`);
 }
 
 computePrice(computer);
