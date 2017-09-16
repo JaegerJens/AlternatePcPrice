cd5efcc60ce2b9e4d6ed10f8a345a92a3cb022cb
Jens Jaeger <jaegerj204@googlemail.com>
2017-09-16 10:17:22 +0200

    add and configure eslint

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ async function getProductDetails(prodId) {
 
 function parsePrice(text) {
     const re = /^\D*(\d+),(\d{2})[\s\S]*/;
-    let val = text.replace(re, "$1.$2");
-    return new Number(val);
+    let val = text.replace(re, '$1.$2');
+    return Number(val);
 }
 
 function getPrice($) {
@@ -31,7 +31,7 @@ function getProductName($) {
 }
 
 async function iterate(obj, action) {
-    for(let key in obj) {
+    for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
             await action(key, obj[key]);
         }
@@ -41,7 +41,7 @@ async function iterate(obj, action) {
 function textLength(obj, propName) {
     if (obj.hasOwnProperty(propName)) {
         const val = obj[propName];
-        if (typeof val === "string") {
+        if (typeof val === 'string') {
             return val.length;
         }
         return val.toString().length;
@@ -50,8 +50,8 @@ function textLength(obj, propName) {
 
 function maxLength(dataset) {
     let res = {};
-    for(let entry of dataset) {
-        for(let key in entry) {
+    for (let entry of dataset) {
+        for (let key in entry) {
             if (entry.hasOwnProperty(key)) {
                 if (res.hasOwnProperty(key)) {
                     let max = Math.max(res[key], textLength(entry, key));
@@ -67,7 +67,7 @@ function maxLength(dataset) {
 
 function pad(obj, prop, len) {
     const val = obj[prop];
-    if (typeof val === "string") {
+    if (typeof val === 'string') {
         return val.padEnd(len[prop]);
     }
     return val.toFixed(2).padStart(len[prop] + 2);
@@ -76,20 +76,20 @@ function pad(obj, prop, len) {
 async function computePrice(computer) {
     let sum = 0.0;
     let data = [];
-    await iterate(computer, async (part, prodId) => {
+    await iterate(computer, async(part, prodId) => {
         const descr = await getProductDetails(prodId);
         const price = getPrice(descr);
         const prodName = getProductName(descr);
         data.push({
-            "type": part,
-            "name": prodName,
-            "price": price
+            'type': part,
+            'name': prodName,
+            'price': price
         });
         sum = sum + price;
     });
     const lengths = maxLength(data);
-    for(let entry of data) {
-        console.log(`${pad(entry, "type", lengths)} ${pad(entry, "name", lengths)} ${pad(entry, "price", lengths)} Euro`);
+    for (let entry of data) {
+        console.log(`${pad(entry, 'type', lengths)} ${pad(entry, 'name', lengths)} ${pad(entry, 'price', lengths)} Euro`);
     }
     console.log('');
     console.log(`TOTAL SUM: ${sum.toFixed(2)} Euro`);
